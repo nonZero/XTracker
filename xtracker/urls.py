a542fc1341a4401d611d3b1470d2c2861819b97b
Udi Oron <udioron@gmail.com>
2017-11-13 17:34:15 +0200

    very simple views

--- xtracker/urls.py
+++ xtracker/urls.py
@@ -15,7 +15,42 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponse
+
+
+def home(request):  # view function
+    assert False, "BOOM!!!!"
+
+def hello(request, name):  # view function
+    return HttpResponse("hello {}!".format(name))
+
+def add(request, a, b):
+    c = int(a) + int(b)
+    return HttpResponse(f"{a} + {b} = {c}")
+
+def age(request, name, i):
+    x = int(i) + 1
+    return HttpResponse(f"{name} is {x} years old.")
+
+# /plus/10/20/
+# 10 + 20 = 30
+
+# /age/danny/20/
+# /age/20/danny/
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
+    url(r'^plus/([0-9]+)/([0-9]+)/$', add),
+    url(r'^hello/(\w+)/$', hello),
+
+    # url(r'^age/(\w+)/([0-9]+)/$', age),
+
+    url(r'^age/(?P<i>[0-9]+)/(?P<name>\w+)/$', age),
+    url(r'^age/(?P<name>\w+)/(?P<i>[0-9]+)/$', age),
+
+    url(r'^age/(?P<name>\w+)/$', age, kwargs={'i': 16}),
+
+    # url(r'^hello/(udi)/$', hello),
+    # url(r'^hello/(yaniv)/$', hello),
+    # url(r'^admin/', admin.site.urls),
 ]
