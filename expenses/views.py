70fd6d7dc14d190eaca9776703ec096c51d81653
Udi Oron <udioron@gmail.com>
2017-11-20 12:47:42 +0200

    Added lotto :-)

--- expenses/views.py
+++ expenses/views.py
@@ -1,15 +1,27 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
-
 
 # random.sample => 6 x 1-42
 # 30% chance "today you are lucky" / "not lucky"
 # random color for each number
 
+COLORS = ['red', 'green', 'yellow', 'blue', 'orange', 'purple', 'cyan',
+          'magenta', 'lime', 'pink', 'teal', 'lavender', 'brown', 'beige',
+          'maroon', 'mint', 'olive', 'coral', 'navy', 'grey', 'white', 'black']
+
+
 def home(request):
+    n = 6
+    lotto = sorted(random.sample(range(1, 43), n))
+    # color = random.choice(COLORS)
+    colors = random.sample(COLORS, n)
+
+    pairs = zip(lotto, colors)
+
     d = {
-        'foo': 'hello world',
-        'bar': 100 * 42 * 2.5,
-        'baz': [91, 45, 111],
+        # 'values': lotto,
+        # 'color': color,
+        'values': pairs,
     }
     return render(request, "expenses/home.html", d)
--- xtracker/urls.py
+++ xtracker/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.http import HttpResponse
 
 urlpatterns = [
     url(r'^expenses/', include("expenses.urls")),
