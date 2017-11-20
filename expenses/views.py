f4a6ce5f5c36f7ff2bffbd9df40fdc0a92b6b255
Udi Oron <udioron@gmail.com>
2017-11-20 11:23:47 +0200

    Added very simple template

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# random.sample => 6 x 1-42
+# 30% chance "today you are lucky" / "not lucky"
+# random color for each number
+
+def home(request):
+    d = {
+        'foo': 'hello world',
+        'bar': 100 * 42 * 2.5,
+        'baz': [91, 45, 111],
+    }
+    return render(request, "expenses/home.html", d)
--- xtracker/urls.py
+++ xtracker/urls.py
@@ -13,44 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.http import HttpResponse
 
-
-def home(request):  # view function
-    assert False, "BOOM!!!!"
-
-def hello(request, name):  # view function
-    return HttpResponse("hello {}!".format(name))
-
-def add(request, a, b):
-    c = int(a) + int(b)
-    return HttpResponse(f"{a} + {b} = {c}")
-
-def age(request, name, i):
-    x = int(i) + 1
-    return HttpResponse(f"{name} is {x} years old.")
-
-# /plus/10/20/
-# 10 + 20 = 30
-
-# /age/danny/20/
-# /age/20/danny/
-
 urlpatterns = [
-    url(r'^$', home),
-    url(r'^plus/([0-9]+)/([0-9]+)/$', add),
-    url(r'^hello/(\w+)/$', hello),
-
-    # url(r'^age/(\w+)/([0-9]+)/$', age),
-
-    url(r'^age/(?P<i>[0-9]+)/(?P<name>\w+)/$', age),
-    url(r'^age/(?P<name>\w+)/(?P<i>[0-9]+)/$', age),
-
-    url(r'^age/(?P<name>\w+)/$', age, kwargs={'i': 16}),
-
-    # url(r'^hello/(udi)/$', hello),
-    # url(r'^hello/(yaniv)/$', hello),
+    url(r'^expenses/', include("expenses.urls")),
     url(r'^admin/', admin.site.urls),
 ]
