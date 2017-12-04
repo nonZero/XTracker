7342f4663a5790d15979727555ff6648d79ed021
Udi Oron <udioron@gmail.com>
2017-12-04 15:27:26 +0200

    Added comments

--- expenses/management/commands/create_expenses.py
+++ expenses/management/commands/create_expenses.py
@@ -2,6 +2,7 @@ import random
 
 import silly
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from expenses.models import Expense
 
@@ -13,7 +14,14 @@ class Command(BaseCommand):
         parser.add_argument('n', type=int)
 
     def handle(self, n, **options):
+
+        # Expense.objects.all().delete()
+
         for i in range(n):
+            self.create_random_expense()
+
+    def create_random_expense(self):
+        with transaction.atomic():
             o = Expense()
             o.title = silly.a_thing()
             o.amount = "{}.{}".format(random.randint(1, 100),
@@ -22,3 +30,9 @@ class Command(BaseCommand):
             o.description = "\n".join(
                 [silly.paragraph(), silly.paragraph(), silly.paragraph()])
             o.save()
+
+            for j in range(random.randint(0, 5)):
+                text = "\n".join(
+                    [silly.paragraph(), silly.paragraph(), silly.paragraph()])
+                o.comments.create(content=text)
+
--- expenses/models.py
+++ expenses/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
 
+# class Tag(models.Model):
+#     name = models.CharField(max_length=100)
+
+
 class Expense(models.Model):
     date = models.DateField()
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     title = models.CharField(max_length=300)
     description = models.TextField(blank=True)
 
+    # tags = models.ManyToManyField(Tag, blank=True, related_name="expenses")
+
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    expense = models.ForeignKey(Expense, related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
